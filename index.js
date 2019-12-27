c4dbf4f738eb20382c10d9872d1f20df74cf55e3
munshiduna <munshiduna@gmail.com>
2019-12-27 06:09:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { ApolloServer, gql, PubSub } from "apollo-server";
 import mongoose from "mongoose";
-
 const Url = mongoose.model("Url", new mongoose.Schema({
 	url: {
 		type: mongoose.Schema.Types.String,
@@ -15,7 +14,6 @@ const Url = mongoose.model("Url", new mongoose.Schema({
 		required: true,
 	},
 }) );
-
 const typeDefs = gql`
 	type Query {
 		urls: [Url!]!
@@ -46,9 +44,7 @@ const typeDefs = gql`
 		urlUpdated: UpdatedField
 	}
 `;
-
 const resolvers = {
-
 	Query: { urls: () => Url.find(), },
 	Url: {
 		url: ( url ) => url.url || "",
@@ -56,45 +52,27 @@ const resolvers = {
 	},
 	Mutation: {
 		createUrl: async (_, { url, title }, { pubSub }) => {
-
 			try {
-
 				const newUrl = new Url({ url, title });
 				const saveUrl = await newUrl.save();
-
 				pubSub.publish("urlCreated", saveUrl);
-
 				return { error: false, message: "OK", data: saveUrl };
-
-			} catch( error ) {
-
+			} catch(error) {
 				return { error: true, message: error.errmsg, data: null };
 			}
 		},
 		deteleUrl: async (_, { urlId }, { pubSub }) => {
-			
 			try {
-
 				const removeUrl = await Url.findOneAndDelete({ _id: urlId });
-
 				pubSub.publish("urlRemoved", removeUrl);
-
 				return { error: false, message: "OK", data: removeUrl };
-
-			} catch( error ) {
-
+			} catch(error) {
 				return { error: true, message: error.message, data: null };
 			}
 		},
 		updateUrl: async (_, { urlId, field, value }, { pubSub }) => {
-
-			const updateUrl = await Url.findOneAndUpdate(
-				{ _id: urlId },
-				{ [field]: value }
-			);
-
+			const updateUrl = await Url.findOneAndUpdate({ _id: urlId }, { [field]: value });
 			pubSub.publish("urlUpdated", { id: urlId, field, value });
-
 			return { error: false, message: "OK", data: updateUrl };
 		},
 	},
@@ -107,38 +85,24 @@ const resolvers = {
 			subscribe: (parent, args, { pubSub }) => pubSub.asyncIterator("urlCreated")
 		},
 		urlRemoved: {
-			resolve: (payload) => {
-				return payload;
-			},
+			resolve: (payload) => payload,
 			subscribe: (parent, args, { pubSub }) => pubSub.asyncIterator("urlRemoved")
 		},
 		urlUpdated: {
-			resolve: (payload) => {
-
-				console.log( payload );
-
-				return payload;
-			},
+			resolve: (payload) => payload,
 			subscribe: (parent, args, { pubSub }) => pubSub.asyncIterator("urlUpdated")
 		},
 	},
 };
-
 async function main () {
-
 	const pubSub = new PubSub()
 	const server = new ApolloServer({ typeDefs, resolvers, context: { pubSub } });
-
 	await mongoose.connect("mongodb://localhost:27017/favurl", {
 		useCreateIndex: true,
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify: true,
 	});
-
-	server.listen().then(({ url }) => {
-		console.log(`Server ready at ${url}`);
-	});
+	server.listen().then(({ url }) => console.log(`Server ready at ${url}`));
 }
-
 main();
